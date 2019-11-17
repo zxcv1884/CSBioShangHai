1ddba2e48b8424ddb41a0b3c1f8deda4378bfbfd
zxcv1884 <zxcv1884@gmail.com>
2019-11-17 18:32:22 +0800

    update header

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 const pages = [
-    {
-        id:1,
-        text:'Home',
-        link:'/',
-    },
+    // {
+    //     id:1,
+    //     text:'Home',
+    //     link:'/',
+    // },
     {
         id:2,
         text:'peptides',
@@ -43,8 +43,9 @@ export default class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            faviconHeight: 60,
+            faviconWidth:60,
             activeElement:null,
-            activeLeft:550,
             left: 550,
             width:0,
             linkStyle:"nav-a font-weight-bolder nav-link py-3",
@@ -64,14 +65,18 @@ export default class Header extends Component {
         }
     };
     handleScroll  = () => {
-        if(window.scrollY<100){
+        if(window.scrollY < 70){
             this.setState({
+                faviconHeight: 60,
+                faviconWidth:60,
                 linkStyle:"nav-a font-weight-bolder nav-link py-3",
                 navStyle:"favicon py-3",
                 hamburgerStyle:"hamburger py-4"
             })
         }else{
             this.setState({
+                faviconHeight: 50,
+                faviconWidth:50,
                 linkStyle:"nav-a font-weight-bolder nav-link",
                 navStyle:"favicon",
                 hamburgerStyle:"hamburger py-3"
@@ -88,10 +93,14 @@ export default class Header extends Component {
         }
     };
     handleMouseClick = (e)=>{
-        let parentUl = e.target.parentNode;
         if (e.target.className.trim().split(" ")[0] === "nav-a") {
+            let parentUl = e.target.parentNode;
             this.setState({
-                activeWidth: parentUl.getBoundingClientRect().width,
+                activeElement: parentUl
+            });
+        }else if(e.target.parentNode.className.trim().split(" ")[0] === "dropdownContentTitle"){
+            let parentUl = e.target.parentNode.parentNode.parentNode.parentNode;
+            this.setState({
                 activeElement: parentUl
             });
         }
@@ -100,36 +109,38 @@ export default class Header extends Component {
         if(this.state.activeElement !== null){
             this.setState({
                 left: this.state.activeElement.getBoundingClientRect().left,
-                width:this.state.activeElement.getBoundingClientRect().width
+                width:this.state.activeElement.getBoundingClientRect().width,
             })
         }else {
             this.setState({
                 left:0,
-                width:0
+                width:0,
             })
         }
     };
 
     render() {
         return (
-            <header className="headerBar fixed-top bg-white" onLoad={this.handleResize}>
+            <header className="headerBar fixed-top bg-white" onLoad={this.handleResize} >
                 <nav className="navbar navbar-light  navbar-expand-lg " style={headerStyle}>
                     <div className={this.state.hamburgerStyle} >
+                        <button className="hamburgerBtn">
                         <img src="assets/img/Hamburger.png" width="30" height="30" className="d-inline-block align-top" alt="" />
+                        </button>
                     </div>
                     <a className={this.state.navStyle} href="/">
-                        <img src="assets/img/favicon.png" width="50" height="50" className="d-inline-block align-top" alt="" />
+                        <img src="assets/img/favicon.png" width={this.state.faviconWidth} height={this.state.faviconHeight} className="faviconPic d-inline-block align-top" alt="" />
                     </a>
                     <div className="collapse navbar-collapse " id="navbarNav" >
                         <ul className="navbar-nav navbar-nav-center mx-auto text-uppercase" >
-                            <div className="stripe" style={{ left: this.state.left , width: this.state.width }}/>
+                            <div className="stripe" style={{ left: this.state.left , width: this.state.width }} />
                             {pages.map((pages) => (
-                                <li className="nav-item  text  position-static"  key={pages.id}  onMouseEnter={this.handleMouseEnter} onClick={this.handleMouseClick} onMouseLeave={this.handleMouseLeave} >
+                                <li className="nav-item text position-static"  key={pages.id}  onMouseEnter={this.handleMouseEnter} onClick={this.handleMouseClick} onMouseLeave={this.handleMouseLeave} >
                                     <Link className={this.state.linkStyle} id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false" to={pages.link}  >{pages.text}</Link>
                                     <ul className="LoadContent animation dropdown-menu vw-100 " >
                                         <div className="form-inline">
                                             <div className="dropdownContentTitle dropdown-content ">
-                                                <Link  to={pages.link} >{pages.text}</Link>
+                                                <Link to={pages.link} onMouseEnter={this.handleMouseEnter}>{pages.text}</Link>
                                             </div>
 
                                         </div>
@@ -142,7 +153,7 @@ export default class Header extends Component {
                         <form className="form-inline border-bottom  search">
                             <input className="form-control border-0 searchTextBox" type="search" placeholder="Search"
                                    aria-label="Search"/>
-                            <button className="btn  my-sm-0 " type="submit" >
+                            <button className="btn  my-sm-0 searchBtn" type="submit" >
                                 <img src="assets/img/header/searchicon.png" width="30" height="30" className="d-inline-block align-top" alt="" />
                             </button>
                         </form>
